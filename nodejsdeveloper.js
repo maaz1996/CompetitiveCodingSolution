701ff693c2762511150ab73a7409afb8f4fbd2ed
Maaz Abdullah <maaz.abdullah111@gmail.com>
2019-12-01 16:45:49 +0530

    -developer community updated code

--- nodejsdeveloper.js
+++ nodejsdeveloper.js
@@ -5,3 +5,42 @@ for(let i=3;i>0;i--)
     console.log(j)
   }
 }
+
+/**
+ * prints
+ * 3
+ * 2
+ * 1
+ * 3
+ * 2
+ * 3
+ * 
+ * 
+ * 
+ * 
+ * but wanted 
+ * 321
+ * 32
+ * 3
+ * 
+ */
+
+
+
+
+
+ /**
+  * 
+  * 
+  * updated code
+  */
+
+ Let X='';
+ For(var i = 1; i <=3 ;i ++){
+ X='';
+  For(var j = 3; j >=i;  j--){
+ X=X+','+j;
+   
+  }
+ console.log(x)
+ }
