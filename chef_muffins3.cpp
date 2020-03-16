d1911fa05d7c68ca9e4b59e463b700013c4474d5
Maaz Abdullah <maaz.abdullah111@gmail.com>
2020-03-17 02:21:39 +0530

    chef_muffins3.cpp

---
+++ chef_muffins3.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long t,n;
+    cin>>t;
+    while(t--)
+    {
+	    cin>>n;
+	    if(n==2) 
+	                cout<<n<<endl;
+	    else
+	               cout<<n/2 +1<<endl;
+    }
+	// your code goes here
+	return 0;
+}
