c91646ba086f5d4024475ead4d47e59b2077a40b
unknown <maaz.abdullah111@gmail.com>
2020-04-25 22:31:43 +0530

    chef_luchtime_april_FFL.cpp

--- chef_lunchtime_FFL.cpp
+++ chef_lunchtime_FFL.cpp
@@ -1,58 +1,59 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
 	// your code goes here
 	long t;
-	cin>>t;
-	while(t--)
-	{ long count=0;
-	    long n,s;
-	    cin>>n>>s;
-	    long a[n],b[n];
-	    
-	    for(int i=0;i<n;i++)
-	    {
-	        cin>>a[i];
-	    }
-	    for(int i=0;i<n;i++)
-	    {
-	        cin>>b[i];
-	    }
-	    
-	    
-	    for(int i=0;i<n;i++)
-	    {
-	        if(b[i]==0){
-	            for(int j=i+1;j<n;j++)
-	            {
-	                if(b[j]==1 && a[i]+a[j]<=(100-s))
-	                {
-	                    count=1;
-	                }
-	            }
-	        }
-	            else if(b[i]==1)
-	            {
-	                for(int j=i+1;j<n;j++){
-	                    if(b[j]==0 && a[i]+a[j]<=(100-s))
-	                    {
-	                        count=1;
-	                    }
-	                }
-	            }
-	        
-	    }
-	    
-	    
-	    if(count==1)
-	    {
-	        cout<<"yes"<<endl;
-	    }
-	     else
-	    {
-	        cout<<"no"<<endl;
-	    }
+	cin >> t;
+	while (t--)
+	{
+		long count = 0;
+		long n, s;
+		cin >> n >> s;
+		long a[n], b[n];
+
+		for (int i = 0; i < n; i++)
+		{
+			cin >> a[i];
+		}
+		for (int i = 0; i < n; i++)
+		{
+			cin >> b[i];
+		}
+
+		for (int i = 0; i < n; i++)
+		{
+			if (b[i] == 0)
+			{
+				for (int j = i + 1; j < n; j++)
+				{
+					if (b[j] == 1 && a[i] + a[j] <= (100 - s))
+					{
+						count = 1;
+					}
+				}
+			}
+			else if (b[i] == 1)
+			{
+				for (int j = i + 1; j < n; j++)
+				{
+					if (b[j] == 0 && a[i] + a[j] <= (100 - s))
+					{
+						count = 1;
+					}
+				}
+			}
+		}
+
+		if (count == 1)
+		{
+			cout << "yes" << endl;
+		}
+		else
+		{
+			cout << "no" << endl;
+		}
 	}
 	return 0;
 }
