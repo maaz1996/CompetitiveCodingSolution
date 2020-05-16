17107e293eaaa5d43f5e09d0844c511a924db7fb
maaz1996 <maaz.abdullah111@gmail.com>
2020-05-17 00:31:32 +0530

    chef_test.cpp

---
+++ chef_test.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // your code goes here
+    int n;
+    while (n != 42)
+    {
+        cin >> n;
+        if (n == 42)
+        {
+            break;
+        }
+        else
+        {
+            cout << n << endl;
+        }
+    }
+    return 0;
+}
+
+//url:https://www.codechef.com/submit/TEST
