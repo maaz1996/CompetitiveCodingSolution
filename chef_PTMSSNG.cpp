87c8f50d5265635c0144463006d2ce8bdd12fefe
maaz1996 <maaz.abdullah111@gmail.com>
2020-07-16 14:02:06 +0530

    updated code; PTMSSNG

--- chef_PTMSSNG.cpp
+++ chef_PTMSSNG.cpp
@@ -1,40 +1,36 @@
 #include <iostream>
 #include <bits/stdc++.h>
+#include <unordered_set>
 using namespace std;
 #define ll long long int
-int main()
-{
-    // your code goes here
-    int t;
-    cin >> t;
-    while (t--)
-    {
-        int n;
-        cin >> n;
-        set<ll> st1, st2;
-
-        for (ll i = 0; i < 4 * n - 1; i++)
-        {
-            ll x, y;
-            cin >> x >> y;
-            if (st1.find(x) != st1.end())
-            {
-                st1.erase(st1.find(x));
-            }
-            else
-                st1.insert(x);
-
-            if (st2.find(y) != st2.end())
-            {
-                st2.erase(st2.find(y));
-            }
-            else
-                st2.insert(y);
-        }
-
-        cout << *st1.begin() << " " << *st2.begin() << endl;
-    }
-    return 0;
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	  
+	    int n;
+	    cin>>n;
+	 unordered_set<ll>st1,st2;
+	
+	    for(ll i=0;i<4*n-1;i++){
+	         ll x,y;
+	        cin>>x>>y;
+	        if(st1.find(x)!=st1.end()){
+	            st1.erase(x);
+	        }
+	        else 
+	        st1.insert(x);
+	        
+	    if(st2.find(y)!=st1.end()){
+	            st2.erase(y);
+	        }
+	        else st2.insert(y);
+	    }
+	    
+	   cout<<*st1.begin()<<" "<<*st2.begin()<<endl;
+	}
+	return 0;
 }
 
 //url:https://www.codechef.com/JULY20B/problems/PTMSSNG/
