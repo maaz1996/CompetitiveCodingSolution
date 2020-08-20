79a24f10a675e5d368b4db736fcaa3619bc94c53
maaz1996 <maaz.abdullah111@gmail.com>
2020-08-21 01:08:08 +0530

    geeksforgeeks:STL

---
+++ Miscellaneous/gfg_stl1.cpp
@@ -0,0 +1,63 @@
+// { Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+//User function Template for C++
+
+/*Function to find frequency of x
+* x : element whose frequency is to be found
+* v : input vector
+*/
+int findFrequency(vector<int> v, int x)
+{
+
+    // Your code here
+    int c = 0;
+    for (int it : v)
+    {
+        if (it == x)
+            c++;
+    }
+    return c;
+}
+
+// { Driver Code Starts.
+
+// Driver code
+int main()
+{
+
+    int testcase;
+
+    cin >> testcase;
+
+    while (testcase--)
+    {
+        int N;
+        cin >> N;
+
+        // Declaring vector
+        vector<int> v;
+
+        // Taking vector element input
+        for (int i = 0; i < N; i++)
+        {
+            int k;
+            cin >> k;
+            v.push_back(k);
+        }
+
+        // element whose frequency to be find
+        int x;
+        cin >> x;
+
+        cout << findFrequency(v, x) << endl;
+    }
+
+    return 0;
+} // } Driver Code Ends
+
+//url:https://practice.geeksforgeeks.org/problems/find-the-frequency/1/
