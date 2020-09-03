3bc1ebe7de6792cd9c17a2d5d772baa7f79ae774
maaz1996 <maaz.abdullah111@gmail.com>
2020-09-03 17:25:12 +0530

    Coding Batch 2020: updated code

--- Miscellaneous/codingbatch2020.cpp
+++ Miscellaneous/codingbatch2020.cpp
@@ -1,8 +1,5 @@
 //maaz.abdullah111@gmail.com
-
 #include <bits/stdc++.h>
-#include <fstream>
-#include <string>
 using namespace std;
 
 int main()
@@ -32,8 +29,21 @@ int main()
         int random_number = rand() % total_lines;
 
         //fetch the line where line index (starting from 0) matches with the random number
-        cout << random_number;
+
         cout << lines[random_number] << endl;
+    }
+
+    int t = 5;
+    string messageVar;
+    cout << "Type your message: " << endl;
+    ;
+    //istream& ignore (streamsize n = 0, int delim = EOF);
+    while (t--)
+    {
+
+        getline(cin, messageVar);
+        cout << "You typed: " << messageVar << endl;
+        cout << messageVar.size() << endl;
     }
     return 0;
 }
